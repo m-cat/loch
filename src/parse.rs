6070ca1a85e958cf83d7d0ffa56b9facf1067e5d
Marcin S <scatman@bu.edu>
2019-03-17 18:40:13 +0100

    fix clippy errors

--- src/parse.rs
+++ src/parse.rs
@@ -31,7 +31,7 @@ pub fn get_urls(line: &str, strategy: Strategy) -> Vec<&str> {
         Strategy::NOHTTP => REGEX_NOHTTP
             .find_iter(line)
             .map(|mat| mat.as_str())
-            .filter(|s| s.contains("/"))
+            .filter(|s| s.contains('/'))
             .collect(),
     }
 }
@@ -48,51 +48,51 @@ mod tests {
                 urls.sort();
                 urls.dedup();
                 assert!(
-                    list_eq(&urls, &$res),
+                    list_eq(&urls, $res),
                     format!("Parsed URLs: {:?}, Expected URLs: {:?}", urls, $res)
                 )
             };
         }
 
-        test_parse!(" http://test", vec!["http://test"]);
+        test_parse!(" http://test", &["http://test"]);
 
-        test_parse!("http://test/.., ", vec!["http://test/"]);
+        test_parse!("http://test/.., ", &["http://test/"]);
 
         test_parse!(
             "http://. http://test. https://example.com-",
-            (vec!["http://test", "https://example.com-"]),
+            &["http://test", "https://example.com-"],
         );
 
-        test_parse!("https://www.google.com/", vec!["https://www.google.com/"]);
+        test_parse!("https://www.google.com/", &["https://www.google.com/"]);
 
-        test_parse!("[https://www.test.com]", vec!["https://www.test.com"]);
+        test_parse!("[https://www.test.com]", &["https://www.test.com"]);
 
         test_parse!(
             "[http://test.asdfasdf/](http://www.test.com)",
-            vec!["http://test.asdfasdf/", "http://www.test.com"],
+            &["http://test.asdfasdf/", "http://www.test.com"],
         );
 
         test_parse!(
             "(http://test)(http:///example)",
-            vec!["http://test", "http:///example"],
+            &["http://test", "http:///example"],
         );
 
         test_parse!(
             "{http://google.com/example}{test}",
-            vec!["http://google.com/example"],
+            &["http://google.com/example"],
         );
 
-        test_parse!("See http://www.test.com.", vec!["http://www.test.com"]);
+        test_parse!("See http://www.test.com.", &["http://www.test.com"]);
 
         test_parse!(
             "https://test.com: a nice website, https://example.com--even nicer",
-            vec!["https://test.com", "https://example.com--even"],
+            &["https://test.com", "https://example.com--even"],
         );
 
         test_parse!(
             "http://www.example.com/page/index.html,www.test.com,https://example.com//, \
              ,example.com/page, ,www.example.com",
-            vec![
+            &[
                 "https://example.com//",
                 "http://www.example.com/page/index.html"
             ],
@@ -100,19 +100,17 @@ mod tests {
 
         test_parse!(
             "http://example.com/#, http://example.com#About",
-            vec!["http://example.com/#", "http://example.com#About"],
+            &["http://example.com/#", "http://example.com#About"],
         );
 
         test_parse!(
             "// https://github.com/maidsafe/QA/blob/master/Documentation/Rust%20Lint%20Checks.md",
-            vec![
-                "https://github.com/maidsafe/QA/blob/master/Documentation/Rust%20Lint%20Checks.md",
-            ],
+            &["https://github.com/maidsafe/QA/blob/master/Documentation/Rust%20Lint%20Checks.md",],
         );
 
         test_parse!(
             "https://www.youtube.com/watch?time_continue=866&v=WGchhsKhG-A",
-            vec!["https://www.youtube.com/watch?time_continue=866&v=WGchhsKhG-A"],
+            &["https://www.youtube.com/watch?time_continue=866&v=WGchhsKhG-A"],
         );
     }
 
@@ -124,56 +122,56 @@ mod tests {
                 urls.sort();
                 urls.dedup();
                 assert!(
-                    list_eq(&urls, &$res),
+                    list_eq(&urls, $res),
                     format!("Parsed URLs: {:?}, Expected URLs: {:?}", urls, $res)
                 )
             };
         }
 
-        let none: Vec<&str> = vec![];
+        let none: &[&str] = &[];
 
-        test_parse!("www.google.com/ ", vec!["www.google.com/"]);
+        test_parse!("www.google.com/ ", &["www.google.com/"]);
 
-        test_parse!("http://test http://test.com/.., ", vec!["http://test.com/"]);
+        test_parse!("http://test http://test.com/.., ", &["http://test.com/"]);
 
-        test_parse!("example.com/page", vec!["example.com/page"]);
+        test_parse!("example.com/page", &["example.com/page"]);
 
         test_parse!(
             "./ test./ test.test() example.com/index",
-            vec!["example.com/index"]
+            &["example.com/index"]
         );
 
         test_parse!("www.google.com", none);
 
-        test_parse!("https://www.google.com/", vec!["https://www.google.com/"]);
+        test_parse!("https://www.google.com/", &["https://www.google.com/"]);
 
         test_parse!(
             "goog.com/page, www.google.com/.",
-            vec!["goog.com/page", "www.google.com/"],
+            &["goog.com/page", "www.google.com/"],
         );
 
-        test_parse!("[https://www.test.com]", vec!["https://www.test.com"]);
+        test_parse!("[https://www.test.com]", &["https://www.test.com"]);
 
         test_parse!(
             "[test.asdfasdf/](http:www.test.asdfasdf)",
-            vec!["test.asdfasdf/"],
+            &["test.asdfasdf/"],
         );
 
-        test_parse!("(test.com/)(test.com)(ftp:///example)", vec!["test.com/"],);
+        test_parse!("(test.com/)(test.com)(ftp:///example)", &["test.com/"],);
 
-        test_parse!("{google.com/example}{test}", vec!["google.com/example"]);
+        test_parse!("{google.com/example}{test}", &["google.com/example"]);
 
-        test_parse!("See www.test.com/.", vec!["www.test.com/"]);
+        test_parse!("See www.test.com/.", &["www.test.com/"]);
 
         test_parse!(
             "test.com/: a nice website, example.com/--even nicer",
-            vec!["example.com/--even", "test.com/"],
+            &["example.com/--even", "test.com/"],
         );
 
         test_parse!(
             "www.example.com/page/index.html,www.test.com,https://example.com, \
              ,example.com/page, ,www.example.com/",
-            vec![
+            &[
                 "example.com/page",
                 "https://example.com",
                 "www.example.com/",
@@ -183,19 +181,17 @@ mod tests {
 
         test_parse!(
             "example.com/#, example.com/#About",
-            vec!["example.com/#", "example.com/#About"],
+            &["example.com/#", "example.com/#About"],
         );
 
         test_parse!(
             "// https://github.com/maidsafe/QA/blob/master/Documentation/Rust%20Lint%20Checks.md",
-            vec![
-                "https://github.com/maidsafe/QA/blob/master/Documentation/Rust%20Lint%20Checks.md",
-            ],
+            &["https://github.com/maidsafe/QA/blob/master/Documentation/Rust%20Lint%20Checks.md",],
         );
 
         test_parse!(
             "www.youtube.com/watch?time_continue=866&v=WGchhsKhG-A",
-            vec!["www.youtube.com/watch?time_continue=866&v=WGchhsKhG-A"],
+            &["www.youtube.com/watch?time_continue=866&v=WGchhsKhG-A"],
         );
     }
 
